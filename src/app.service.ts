313ff97e6864ad00bc4c530ba63989ec2c3eccd8
kevinefraim <kevinefraim02@gmail.com>
2023-01-26 16:28:27 -0300

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! CHANGES';
+    return 'Hello World! CHANGES 2';
   }
 }
